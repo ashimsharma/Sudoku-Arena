a2cb6731ccbc9730d4bf01ce71df9c77cb92eb39
ashimsharma <sharmaashim756@gmail.com>
2025-02-17 01:39:01 -0500

    Delete dist

--- backend/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-app_1.app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server running on port ${process.env.PORT || 3000}`);
-});
-//# sourceMappingURL=index.js.map
