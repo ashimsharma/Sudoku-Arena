08e5730a66f548438675c63c9c78086617633bc7
ashimsharma <sharmaashim756@gmail.com>
2025-02-27 09:15:22 -0500

    Update .gitignore

--- websocket-server/dist/messages/messages.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JOIN_ROOM = exports.CREATE_ROOM = void 0;
-exports.CREATE_ROOM = "create_room";
-exports.JOIN_ROOM = "join_room";
--- websocket-server/dist/store/connections.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectionUserIds = void 0;
-exports.connectionUserIds = new Map();
