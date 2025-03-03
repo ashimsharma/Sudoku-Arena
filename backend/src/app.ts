a162871f1a7539a6322fb021d7bb0eb4d33200ba
ashimsharma <sharmaashim756@gmail.com>
2025-03-02 20:42:18 -0500

    Add Cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,19 @@ import express from "express";
 import 'dotenv/config';
 import authRouter from "./routes/auth";
 import passport from "./config/passport";
+import cors from 'cors';
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
